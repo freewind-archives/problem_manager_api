aa283c85ae47dc7872b2298e1a21b0cc3ab9bd5c
Li Dong <dongli@thoughtworks.com>
2014-07-09 18:10:41 +0800

    #4, adding hello action to respond /diagnostic/hello post request. Pair: Sarah & Dong

--- app/controllers/Diagnostic.scala
+++ app/controllers/Diagnostic.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import play.api.mvc._
+import play.api.libs.json._
 
 object Diagnostic extends Controller {
 
@@ -10,6 +11,12 @@ object Diagnostic extends Controller {
 
   def name = Action {
     Ok("Problem Manager Api")
+  }
+
+  def hello = Action (BodyParsers.parse.json) { request =>
+    val name = (request.body \ "name").asOpt[String]
+    val response = Json.obj("hello" -> name)
+    Ok(response)
   }
 
 }
--- test/controllers/DiagnosticSpec.scala
+++ test/controllers/DiagnosticSpec.scala
@@ -5,6 +5,7 @@ import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
 import play.api.test.Helpers._
 import play.api.test.{FakeRequest, WithApplication}
+import play.api.libs.json._
 
 @RunWith(classOf[JUnitRunner])
 class DiagnosticSpec extends Specification {
@@ -16,11 +17,19 @@ class DiagnosticSpec extends Specification {
       status(ping) must equalTo(OK)
       contentAsString(ping) must contain ("ok")
     }
-    "render =Problem Manager Api= when visit name" in new WithApplication{
+    "render =Problem Manager Api= when visit name" in new WithApplication {
       val ping = route(FakeRequest(GET, "/diagnostic/name")).get
 
       status(ping) must equalTo(OK)
-      contentAsString(ping) must contain ("Problem Manager Api")
+      contentAsString(ping) must contain("Problem Manager Api")
+    }
+
+    "return hello message when recieve a post request with json data" in new WithApplication{
+      val hello = route(FakeRequest(POST, "/diagnostic/hello").withJsonBody(Json.parse("""{ "name": "value" }"""))).get
+
+      status(hello) must equalTo(OK)
+
+      contentAsJson(hello) mustEqual Json.obj("hello" -> "value")
     }
   }
 
