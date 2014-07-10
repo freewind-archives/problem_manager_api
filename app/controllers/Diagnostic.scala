24dee83ab71e6158c97d1dbdc1784a8489b8146b
cuipengfei <cuipengfei2008@gmail.com>
2014-07-11 00:27:47 +0800

    #3 实现版本号。没找到合适的before after的方式。。。。。。

--- app/controllers/Diagnostic.scala
+++ app/controllers/Diagnostic.scala
@@ -2,8 +2,12 @@ package controllers
 
 import play.api.mvc._
 import play.api.libs.json._
+import scala.io.Source.fromFile
+import java.nio.file.{Paths, Files}
 
 object Diagnostic extends Controller {
+
+  val BUILD_VERSION_FILE = "build_version"
 
   def ping: Action[AnyContent] = Action {
     Ok("ok")
@@ -19,4 +23,15 @@ object Diagnostic extends Controller {
     Ok(response)
   }
 
+  def version: Action[AnyContent] = Action {
+    Ok(readVersionOrElse("unknown"))
+  }
+
+  private def readVersionOrElse(alternative: String): String = {
+    if (Files.exists(Paths.get(BUILD_VERSION_FILE))) {
+      fromFile(BUILD_VERSION_FILE).mkString
+    } else {
+      alternative
+    }
+  }
 }
--- test/controllers/DiagnosticSpec.scala
+++ test/controllers/DiagnosticSpec.scala
@@ -6,9 +6,12 @@ import org.specs2.runner.JUnitRunner
 import play.api.test.Helpers._
 import play.api.test.{FakeRequest, WithApplication}
 import play.api.libs.json._
+import scalax.io._
+import scalax.file.Path
 
 @RunWith(classOf[JUnitRunner])
 class DiagnosticSpec extends Specification {
+
   "Diagnostic" should {
 
     "render ok when visit ping" in new WithApplication {
@@ -17,11 +20,30 @@ class DiagnosticSpec extends Specification {
       status(ping) === OK
       contentAsString(ping) must contain("ok")
     }
+
     "render =Problem Manager Api= when visit name" in new WithApplication {
       val ping = route(FakeRequest(GET, "/diagnostic/name")).get
 
       status(ping) === OK
       contentAsString(ping) must contain("Problem Manager Api")
+    }
+
+    "return build version when build version file exists" in new WithApplication {
+      cleanUpFile
+      Resource.fromFile("build_version").write("1.0.0")
+      val versionResp = route(FakeRequest(GET, "/diagnostic/version")).get
+
+      status(versionResp) === OK
+      contentAsString(versionResp) === "1.0.0"
+      cleanUpFile
+    }
+
+    "return unknown when build version file does notexists" in new WithApplication {
+      cleanUpFile
+      val versionResp = route(FakeRequest(GET, "/diagnostic/version")).get
+
+      status(versionResp) === OK
+      contentAsString(versionResp) === "unknown"
     }
 
     "return hello message when recieve a post request with json data" in new WithApplication {
@@ -31,6 +53,9 @@ class DiagnosticSpec extends Specification {
       contentType(hello) must beSome("application/json")
       contentAsJson(hello) mustEqual Json.obj("hello" -> "value")
     }
+
+    private def cleanUpFile = Path("build_version").deleteIfExists()
+
   }
 
 }
